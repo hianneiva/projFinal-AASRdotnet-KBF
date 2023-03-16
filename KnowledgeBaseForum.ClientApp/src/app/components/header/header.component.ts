724e4b0d357c71efc671567c35442a469b7886dc
hianneiva <hianneiva@gmail.com>
2023-03-16 14:36:28 -0300

    Correções de atualização de cabeçalho e estilo
     - Correção de tratamento da atualização de cabeçalho em log off;
     - Ajuste no estilo da interface.

--- KnowledgeBaseForum.ClientApp/src/app/components/header/header.component.ts
+++ KnowledgeBaseForum.ClientApp/src/app/components/header/header.component.ts
@@ -16,6 +16,7 @@ export class HeaderComponent {
 
   public logoff(): void {
     this.cookie.delete(environment.cookieToken);
+    this.userData = undefined;
     this.router.navigate(['/'])/*.then(() => { window.location.reload(); })*/;
   }
 }
